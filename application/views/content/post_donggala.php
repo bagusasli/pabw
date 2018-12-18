0f7b19ac8444884aac54ff92b9080c3925d0f66e
Ramadhana Bagus <17523044@students.uii.ac.id>
2018-12-18 07:18:18 +0700

    CDN LAGI

--- application/views/content/post_donggala.php
+++ application/views/content/post_donggala.php
@@ -18,7 +18,7 @@ $b=$data->row_array();
 		<link type="text/css" rel="stylesheet" href="<?php echo $actual_link ?>webmag/css/bootstrap.min.css"/>
 
 		<!-- Font Awesome Icon -->
-		<link rel="stylesheet" href="<?php echo $actual_link ?>webmag/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
 
 		<!-- Custom stlylesheet -->
 		<link type="text/css" rel="stylesheet" href="<?php echo $actual_link ?>webmag/css/style.css"/>
--- application/views/content/post_index.php
+++ application/views/content/post_index.php
@@ -18,7 +18,7 @@ $actual_link = "http://localhost/bigpro/";
     <link rel="stylesheet" href="<?php echo $actual_link ?>isotope-master/css/style.css" />
 
 		<!-- Font Awesome Icon -->
-		<link rel="stylesheet" href="<?php echo $actual_link ?>webmag/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
 
 		<!-- Custom stlylesheet -->
 		<link type="text/css" rel="stylesheet" href="<?php echo $actual_link ?>webmag/css/style.css"/>
